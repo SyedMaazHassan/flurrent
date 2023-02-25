2fa1160178f9bc0b167fa2f4dbcae2441e77591d
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-25 19:45:18 +0500

    add: populated refer_id of previous users

--- authentication/views.py
+++ authentication/views.py
@@ -6,14 +6,18 @@ from organizations.models import Organization
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.http import HttpResponse
+import random
+import uuid
+
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("core:home")
 
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
@@ -22,27 +26,27 @@ def login_view(request):
             context = {
                 "email": email,
                 "password": password,
-                "error": 'Invalid email or password'
+                "error": "Invalid email or password",
             }
-            return render(request, 'signin.html', context)
+            return render(request, "signin.html", context)
     else:
-        return render(request, 'signin.html')
+        return render(request, "signin.html")
 
 
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect('authentication:login')
+    return redirect("authentication:login")
 
 
 def validatePersonalInfo(request):
     status = False
     message = ""
-    first_name = request.POST.get('first_name')
-    last_name = request.POST.get('last_name')
-    email = request.POST.get('email')
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    email = request.POST.get("email")
     if first_name and last_name and email:
-        if not User.objects.filter(email = email).exists():
+        if not User.objects.filter(email=email).exists():
             status = True
         else:
             message = "Email address already exist"
@@ -50,26 +54,22 @@ def validatePersonalInfo(request):
     return {
         "status": status,
         "message": message,
-        "data": {
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email
-        }
+        "data": {"first_name": first_name, "last_name": last_name, "email": email},
     }
+
 
 def validateProfileType(request):
     status = False
     message = ""
-    profile_type        = request.POST.get('profile_type')
-    organization_name   = request.POST.get('organization_name')
-    organization_type   = request.POST.get('organization_type')
-    endorser_tagline    = request.POST.get('endorser_tagline') 
-    endorser_followers  = request.POST.get('endorser_followers') 
-    
- 
+    profile_type = request.POST.get("profile_type")
+    organization_name = request.POST.get("organization_name")
+    organization_type = request.POST.get("organization_type")
+    endorser_tagline = request.POST.get("endorser_tagline")
+    endorser_followers = request.POST.get("endorser_followers")
+
     if profile_type == "organization":
         if organization_name and organization_type:
-            if not Organization.objects.filter(name = organization_name).exists():
+            if not Organization.objects.filter(name=organization_name).exists():
                 status = True
             else:
                 message = "Organization already exist"
@@ -90,16 +90,16 @@ def validateProfileType(request):
             "organization_name": organization_name,
             "organization_type": organization_type,
             "endorser_tagline": endorser_tagline,
-            "endorser_followers": endorser_followers
-            
-        }
+            "endorser_followers": endorser_followers,
+        },
     }
+
 
 def validatePassword(request):
     status = False
     message = ""
-    password1 = request.POST.get('password1')
-    password2 = request.POST.get('password2')
+    password1 = request.POST.get("password1")
+    password2 = request.POST.get("password2")
     if password1 and password2 and password1 == password2:
         status = True
     else:
@@ -108,13 +108,8 @@ def validatePassword(request):
     return {
         "status": status,
         "message": message,
-        "data": {
-            "password1": password1,
-            "password2": password2
-        }
+        "data": {"password1": password1, "password2": password2},
     }
-
-
 
 
 def signup_view(request):
@@ -128,17 +123,19 @@ def signup_view(request):
         print(profile_type)
         print(set_password)
 
-        
-        
-        if personal_info["status"] and profile_type["status"] and set_password["status"]:
+        if (
+            personal_info["status"]
+            and profile_type["status"]
+            and set_password["status"]
+        ):
             # Create profile
             try:
                 with transaction.atomic():
                     # creating new user:
                     new_user = User(
-                        first_name = personal_info["data"]["first_name"],
-                        last_name = personal_info["data"]["last_name"],
-                        email = personal_info["data"]["email"]
+                        first_name=personal_info["data"]["first_name"],
+                        last_name=personal_info["data"]["last_name"],
+                        email=personal_info["data"]["email"],
                     )
                     new_user.set_password(set_password["data"]["password1"])
                     new_user.save()
@@ -146,25 +143,25 @@ def signup_view(request):
                     # Creating profile type
                     if profile_type["data"]["profile_type"] == "organization":
                         new_user.createOrganization(
-                            name = profile_type["data"]["organization_name"],
-                            type = profile_type["data"]["organization_type"]
+                            name=profile_type["data"]["organization_name"],
+                            type=profile_type["data"]["organization_type"],
                         )
                         # Organization created
 
                     # Creating profile type
                     if profile_type["data"]["profile_type"] == "endorser":
                         new_user.createEndorser(
-                            tagline = profile_type["data"]["endorser_tagline"],
-                            followers = profile_type["data"]["endorser_followers"]
+                            tagline=profile_type["data"]["endorser_tagline"],
+                            followers=profile_type["data"]["endorser_followers"],
                         )
                         # endorser created
 
                     messages.success(request, "Profile has been created successfully!")
                     return redirect("authentication:login")
-            
+
             except Exception as e:
                 messages.error(request, str(e))
-            
+
         else:
             error_msg = ""
             if personal_info["message"]:
@@ -177,12 +174,12 @@ def signup_view(request):
                 error_msg += "»" + set_password["message"] + "\n"
 
             messages.error(request, error_msg)
-        
+
         context = {
             "error": True,
             "personal_info": personal_info,
             "profile_type": profile_type,
-            "set_password": set_password
+            "set_password": set_password,
         }
 
-    return render(request, 'signup.html', context)
+    return render(request, "signup.html", context)
