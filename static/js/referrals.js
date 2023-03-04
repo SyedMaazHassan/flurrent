5b0c7b8fb35335a24888ae734f48a24d983449cf
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-03-04 15:04:14 +0500

    fix: copyText()

--- static/js/referrals.js
+++ static/js/referrals.js
@@ -4,6 +4,10 @@ copyText = async (link) => {
     const success_alert__container = document.getElementById("success_alert__container")
     success_alert__container.classList.remove("d-none")
     success_alert__container.classList.add("d-block")
+    setTimeout(() => {
+      success_alert__container.classList.remove("d-block")
+      success_alert__container.classList.add("d-none")
+    }, 3000)
   } catch (err) {
     console.log(err)
   }
